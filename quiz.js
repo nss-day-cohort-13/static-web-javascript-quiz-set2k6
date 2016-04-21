b7f41fe1ed038b453c3f35b8b2b2487700e6c896
set2k6 <set2k6@gmail.com>
2016-04-21 15:27:34 -0500

    initial commit. trying to add event listeners. added some more things for reference later on comments

---
+++ quiz.js
@@ -0,0 +1,17 @@
+
+var character = ["!", "+", "-", ".", "?", "*", "&", "^", "%", "$", "#", "@"];
+var char = ""
+
+
+if ()
+
+for (var i = 0; i < character.length; i++) {
+	 character[i]
+}
+document.getElementById("hBut").addEventListener("click", function(){
+    console.log(char);
+});
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
